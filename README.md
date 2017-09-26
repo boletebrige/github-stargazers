d4401c8beae562ae5b5c3c8c3d1d845541a0ac46
boletebrige <josip.ravas.broj@gmail.com>
2017-09-26 02:47:49 +0200

    tabs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Task
+## Don't load reload data
 # Material
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.2.
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,6 +18,30 @@ export class HomeComponent {
       {user: 'tastejs', repo: 'todomvc'},
       {user: 'Polymer', repo: 'polymer'}
     ];
+    webArr = [
+      {user: 'meteor', repo: 'meteor'},
+      {user: 'rails', repo: 'rails'},
+      {user: 'laravel', repo: 'laravel'},
+      {user: 'expressjs', repo: 'express'},
+      {user: 'pallets', repo: 'flask'},
+      {user: 'django', repo: 'django'}
+    ];
+    gameArr = [
+      {user: 'photonstorm', repo: 'phaser'},
+      {user: 'pixijs', repo: 'pixi.js'},
+      {user: 'BabylonJS', repo: 'Babylon.js'},
+      {user: 'WhitestormJS', repo: 'whs.js'},
+      {user: 'wellcaffeinated', repo: 'PhysicsJS'},
+      {user: 'playcanvas', repo: 'engine'}
+    ];
+    textArr = [
+      {user: 'atom', repo: 'atom'},
+      {user: 'Microsoft', repo: 'vscode'},
+      {user: 'adobe', repo: 'brackets'},
+      {user: 'neovim', repo: 'neovim'},
+      {user: 'limetext', repo: 'lime'},
+      {user: 'syl20bnr', repo: 'spacemacs'}
+    ];
     dataArr = [];
     input = {
       user: null,
@@ -35,13 +59,31 @@ export class HomeComponent {
     ngOnInit() {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
         //Add 'implements OnInit' to the class.
-        for(let i = 0; i < this.initialArr.length; i++){
-          this.http.get('https://api.github.com/repos/' + this.initialArr[i].user + '/'+ this.initialArr[i].repo)
-          .subscribe(data => {
-            this.dataArr.push(data.json());
-            this.dataArr.sort(function(a, b){return b.stargazers_count- a.stargazers_count})
-          });
-        }
-        this.dataLoaded = !this.dataLoaded;
+        this.getData(this.initialArr)
+        console.log('dataArr', this.dataArr)
+    }
+    getData(inputArr){
+      this.dataArr = [];
+      for(let i = 0; i < inputArr.length; i++){
+        this.http.get('https://api.github.com/repos/' + inputArr[i].user + '/'+ inputArr[i].repo)
+        .subscribe(data => {
+          this.dataArr.push(data.json());
+          this.dataArr.sort(function(a, b){return b.stargazers_count- a.stargazers_count})
+        });
+      }
+      this.dataLoaded = !this.dataLoaded;
+      console.log(this.dataArr)
+    }
+    onSelectChange = ($event: any): void => {
+      if($event.index == 1){
+        this.getData(this.webArr)
+      }else if($event.index == 2){
+        this.getData(this.gameArr)
+      }else if($event.index == 3){
+        this.getData(this.textArr)
+      }
+      else if($event.index == 0){
+        this.getData(this.initialArr)
+      }
     }
   }
